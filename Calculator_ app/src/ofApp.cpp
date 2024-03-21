f1f7f06a191afa630460377f2562ee7b92437bf7
Anjali-M22 <115711554+Anjali-M22@users.noreply.github.com>
2024-03-21 15:44:54 +0000

    setup for HEX page and drew the HEX button

--- Calculator_ app/src/ofApp.cpp
+++ Calculator_ app/src/ofApp.cpp
@@ -25,12 +25,11 @@ void ofApp::setup() {
 
 
 	//------------ANJALI--------------------
-	//buttonInterface.set(20, 20, 460, 100); // an idea for the size of the interface
 	buttonClear.set(20, 140, 100, 100);
 	button7.set(20, 260, 100, 100);
 	button4.set(20, 380, 100, 100);
 	buttonPandN.set(20, 620, 100, 100); // plus and minus sign for positive and negative numbers
-
+	buttonHEX.set(140, 140, 100, 100);
 
 	//--------------------------------------
 
@@ -41,6 +40,22 @@ void ofApp::setup() {
 	button9.set(260, 260, 100, 100);
 	button6.set(260, 380, 100, 100);
 	buttonPercentage.set(260, 140, 100, 100);
+	//--------------------------------------
+
+	//--------buttons for the HEX page-----------------
+
+
+
+	//------------ANJALY--------------------
+
+
+	//--------------------------------------
+
+	
+
+	//------------ANJALI--------------------
+
+
 	//--------------------------------------
 
 
@@ -54,92 +69,114 @@ void ofApp::update() {
 //--------------------------------------------------------------
 void ofApp::draw() {
 
-	ofBackground(0);
-	ofSetColor(180);
-	//draw button
-	//---------KIERAN----------------------
-	ofDrawRectangle(button1);
-	ofSetColor(255, 165, 0); //changed the colour to match the other operators(DAN)
-	ofDrawRectangle(buttonEqual); //Equal button, Decimal place button and number 0 drawn by Kieran
-	ofSetColor(180);
-	ofDrawRectangle(buttonDecimal);
-	ofDrawRectangle(button0);
-	//-------------------------------------
+	// Draw different elements based on the current screen state
+	switch (currentScreen) {
+		case MAIN_SCREEN:
+			
+			ofBackground(0);
+			ofSetColor(180);
+			//draw button
+			//---------KIERAN----------------------
+			ofDrawRectangle(button1);
+			ofSetColor(255, 165, 0); //changed the colour to match the other operators(DAN)
+			ofDrawRectangle(buttonEqual); //Equal button, Decimal place button and number 0 drawn by Kieran
+			ofSetColor(180);
+			ofDrawRectangle(buttonDecimal);
+			ofDrawRectangle(button0);
+			//-------------------------------------
 
 
-	//------------DAN----------------------
-	ofSetColor(255, 165, 0);
-	ofDrawRectangle(buttonDiv);
-	ofDrawRectangle(buttonX);
-	ofDrawRectangle(buttonMinus);
-	ofDrawRectangle(buttonPlus);
-	ofSetColor(180);
-	ofDrawRectangle(button3);
+			//------------DAN----------------------
+			ofSetColor(255, 165, 0);
+			ofDrawRectangle(buttonDiv);
+			ofDrawRectangle(buttonX);
+			ofDrawRectangle(buttonMinus);
+			ofDrawRectangle(buttonPlus);
+			ofSetColor(180);
+			ofDrawRectangle(button3);
 
-	//--------------------------------------
-
-
-	//------------ANJALI--------------------
-	ofDrawRectangle(buttonClear);
-	ofDrawRectangle(button7);
-	ofDrawRectangle(button4);
-	ofDrawRectangle(buttonPandN);
-	//ofDrawRectangle(buttonInterface);
-
-	//--------------------------------------
-
-	//------------ANJALY--------------------
-	ofDrawRectangle(button8);
-	ofDrawRectangle(button5);
-	ofDrawRectangle(button2);
-	ofDrawRectangle(button9);
-	ofDrawRectangle(button6);
-	ofDrawRectangle(buttonPercentage);
-	//--------------------------------------
+			//--------------------------------------
 
 
+			//------------ANJALI--------------------
+			ofDrawRectangle(buttonClear);
+			ofDrawRectangle(button7);
+			ofDrawRectangle(button4);
+			ofDrawRectangle(buttonPandN);
+			ofDrawRectangle(buttonHEX);
+
+			//--------------------------------------
+
+			//------------ANJALY--------------------
+			ofDrawRectangle(button8);
+			ofDrawRectangle(button5);
+			ofDrawRectangle(button2);
+			ofDrawRectangle(button9);
+			ofDrawRectangle(button6);
+			ofDrawRectangle(buttonPercentage);
+			//--------------------------------------
 
 
-	//---------KIERAN----------------------
-	ofSetColor(255);
-	//adds the text for each number
-	testFont.drawString("1", 60, 570);
-	testFont.drawString(".", 305, 680); //Decimal point drawn
-	testFont.drawString("0", 180, 690); //Number 0 drawn
-	testFont.drawString("=", 420, 690); //Equals operator drawn
-	//-------------------------------------
 
 
-	//------------DAN----------------------
-	testFont.drawString("/", 415, 210);
-	testFont.drawString("X", 420, 330);
-	testFont.drawString("-", 420, 450);
-	testFont.drawString("+", 415, 570);
-	testFont.drawString("3", 300, 570);
-
-	//--------------------------------------
-
-
-	//------------ANJALI--------------------
-	testFont.drawString("C", 55, 210); // Clear number drawn
-	testFont.drawString("7", 55, 330);
-	testFont.drawString("4", 55, 450);
-	testFont.drawString("+/-", 30, 690); // plus and minus sign drawn
+			//---------KIERAN----------------------
+			ofSetColor(255);
+			//adds the text for each number
+			testFont.drawString("1", 60, 570);
+			testFont.drawString(".", 305, 680); //Decimal point drawn
+			testFont.drawString("0", 180, 690); //Number 0 drawn
+			testFont.drawString("=", 420, 690); //Equals operator drawn
+			//-------------------------------------
 
 
-	//--------------------------------------
+			//------------DAN----------------------
+			testFont.drawString("/", 415, 210);
+			testFont.drawString("X", 420, 330);
+			testFont.drawString("-", 420, 450);
+			testFont.drawString("+", 415, 570);
+			testFont.drawString("3", 300, 570);
 
-	//------------ANJALY--------------------
-		   //adds the text for each number
-	testFont.drawString("8", 180, 330);
-	testFont.drawString("5", 180, 450);
-	testFont.drawString("2", 180, 570);
-	testFont.drawString("9", 300, 330);
-	testFont.drawString("6", 300, 450);
-	testFont.drawString("%", 285, 210);
+			//--------------------------------------
 
 
-	//--------------------------------------
+			//------------ANJALI--------------------
+			testFont.drawString("C", 55, 210); // Clear number drawn
+			testFont.drawString("7", 55, 330);
+			testFont.drawString("4", 55, 450);
+			testFont.drawString("+/-", 30, 690); // plus and minus sign drawn
+			testFont.drawString("HEX", 150, 210);
+
+
+			//--------------------------------------
+
+			//------------ANJALY--------------------
+				   //adds the text for each number
+			testFont.drawString("8", 180, 330);
+			testFont.drawString("5", 180, 450);
+			testFont.drawString("2", 180, 570);
+			testFont.drawString("9", 300, 330);
+			testFont.drawString("6", 300, 450);
+			testFont.drawString("%", 285, 210);
+
+
+			//--------------------------------------
+		
+		break;
+		//--------------------------------------
+		case HEX_SCREEN:
+			ofBackground(0);
+			ofSetColor(180);
+			
+			ofDrawRectangle(buttonHEX);
+		
+			//--------------------------------------
+			ofSetColor(255);
+			testFont.drawString("HEX", 150, 210);
+		
+		        //--------------------------------------
+		break;
+	}
+		
 }
 
 //--------------------------------------------------------------
@@ -165,77 +202,93 @@ void ofApp::mouseDragged(int x, int y, int button) {
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button) {
 
-	if (button1.inside(x, y)) {
-		appendNumber("1"); // Append "1" to the current number
-	}
+	switch (currentScreen) {
+		case MAIN_SCREEN:
+			if (buttonHEX.inside(x, y)) {
+				// Switch to the hex screen
+				currentScreen = HEX_SCREEN;
+			}
+			if (button1.inside(x, y)) {
+				appendNumber("1"); // Append "1" to the current number
+			}
+			if (button2.inside(x, y)) {
+				appendNumber("2"); // Append "2" to the current number
+			}
+			if (button3.inside(x, y)) {
+				appendNumber("3"); // Append "3" to the current number
+			}
+			if (button4.inside(x, y)) {
+				appendNumber("4"); // Append "4" to the current number
+			}
+			if (button5.inside(x, y)) {
+				appendNumber("5"); // Append "5" to the current number
+			}
+			if (button6.inside(x, y)) {
+				appendNumber("6"); // Append "6" to the current number
+			}
+			if (button7.inside(x, y)) {
+				appendNumber("7"); // Append "7" to the current number
+			}
+			if (button8.inside(x, y)) {
+				appendNumber("8"); // Append "8" to the current number
+			}
+			if (button9.inside(x, y)) {
+				appendNumber("9"); // Append "9" to the current number
+			}
+			if (button0.inside(x, y)) {
+				appendNumber("0"); // Append "0" to the current number
+			}
 
-	if (button2.inside(x, y)) {
-		appendNumber("2"); // Append "2" to the current number
-	}
+			if (buttonPlus.inside(x, y)) {
+				processOperator("+"); // Process addition operator
+			}
 
-	if (button3.inside(x, y)) {
-		appendNumber("3"); // Append "3" to the current number
-	}
-	if (button4.inside(x, y)) {
-		appendNumber("4"); // Append "4" to the current number
-	}
-	if (button5.inside(x, y)) {
-		appendNumber("5"); // Append "5" to the current number
-	}
-	if (button6.inside(x, y)) {
-		appendNumber("6"); // Append "6" to the current number
-	}
-	if (button7.inside(x, y)) {
-		appendNumber("7"); // Append "7" to the current number
-	}
-	if (button8.inside(x, y)) {
-		appendNumber("8"); // Append "8" to the current number
-	}
-	if (button9.inside(x, y)) {
-		appendNumber("9"); // Append "9" to the current number
-	}
-	if (button0.inside(x, y)) {
-		appendNumber("0"); // Append "0" to the current number
-	}
+			if (buttonDecimal.inside(x, y)) {
+				appendNumber("."); // Process decimal operator
+			}
 
-	if (buttonPlus.inside(x, y)) {
-		processOperator("+"); // Process addition operator
-	}
+			if (buttonDiv.inside(x, y)) {
+				processOperator("/"); // Process divison operator
+			}
 
-	if (buttonDecimal.inside(x, y)) {
-		appendNumber("."); // Process decimal operator
-	}
+			if (buttonX.inside(x, y)) {
+				processOperator("*"); // Process multiplication operator
+			}
 
-	if (buttonDiv.inside(x, y)) {
-		processOperator("/"); // Process divison operator
-	}
+			if (buttonEqual.inside(x, y)) {
+				calculate(); // Calculate the result
+			}
 
-	if (buttonX.inside(x, y)) {
-		processOperator("*"); // Process multiplication operator
-	}
+			if (buttonMinus.inside(x, y)) {
+				processOperator("-"); // process substraction operator
+			}
+			
+			if (buttonPandN.inside(x, y)) {
+				appendNumber("-"); //negative integers
+			}
 
-	if (buttonEqual.inside(x, y)) {
-		calculate(); // Calculate the result
-	}
+			if (buttonPercentage.inside(x, y)) {
+				percentage(); // Process percentage function
+			}
 
-	if (buttonMinus.inside(x, y)) {
-		processOperator("-"); // process substraction operator
-	}
-	if (buttonPandN.inside(x, y)) {
-		appendNumber("-"); //negative integers
-	}
+			if (buttonClear.inside(x, y)) {
+				currentNum.clear(); // Clear the current number
+				totalNum.clear(); // Clear the total number
+				operatorState.clear(); // Clear the operator state
+				runningTotal = 0; // Reset the running total to zero
+				calculationPerformed = false; // Reset the flag for calculation performed
+				return; // Return to avoid executing other button checks
+			}
 
-	if (buttonPercentage.inside(x, y)) {
-		percentage(); // Process percentage function
-	}
-
-		if (buttonClear.inside(x, y)) {
-		currentNum.clear(); // Clear the current number
-		totalNum.clear(); // Clear the total number
-		operatorState.clear(); // Clear the operator state
-		runningTotal = 0; // Reset the running total to zero
-		calculationPerformed = false; // Reset the flag for calculation performed
-		return; // Return to avoid executing other button checks
+		break;
+		//--------------------------------------
+		case HEX_SCREEN:
+			
+			if (buttonHEX.inside(x, y)) {
+				// Switch to the main screen
+				currentScreen = MAIN_SCREEN;
+			}
+		break;
 	}
 }
 
