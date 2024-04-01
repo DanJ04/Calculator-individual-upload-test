0ede6c65aff63d64e17180c427273f7bc772863f
Anjali-M22 <115711554+Anjali-M22@users.noreply.github.com>
2024-04-01 13:16:05 +0100

    deleted unnecessary functions

--- Calculator_ app/src/ofApp.h
+++ Calculator_ app/src/ofApp.h
@@ -4,154 +4,138 @@
 #include "ofxGui.h"
 #include "ofxJSON.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
+public:
 
-		//--------------------------------------
-		// a new screen for the HEX page
-		enum ScreenState {
-			MAIN_SCREEN,
-			HEX_SCREEN,
-		};
+	//--------------------------------------
+	// a new screen for the HEX page
+	enum ScreenState {
+		MAIN_SCREEN,
+		HEX_SCREEN,
+	};
 
-		// always start with the main screen
-		ScreenState currentScreen = MAIN_SCREEN;
+	// always start with the main screen
+	ScreenState currentScreen = MAIN_SCREEN;
 
-		//--------------------------------------
-
-
-		void setup();
-		void update();
-		void draw();
-
-		ofTrueTypeFont testFont;
-		ofSoundPlayer resultSound;
-                //---------KIERAN----------------------
-		ofRectangle button1;
-		ofRectangle buttonEqual;
-		ofRectangle buttonDecimal;
-		ofRectangle button0;
-		ofRectangle historyButton;
-		//-------------------------------------
+	//--------------------------------------
 
 
-		//------------DAN----------------------
-		ofRectangle buttonPlus;
-		ofRectangle buttonX;
-		ofRectangle buttonMinus;
-		ofRectangle buttonDiv; 
-		ofRectangle button3;
+	void setup();
+	void draw();
 
-		//--------------------------------------
-
-
-		//------------ANJALI--------------------
-                ofRectangle buttonClear;
-                ofRectangle button7;
-                ofRectangle button4;
-                ofRectangle buttonPandN; // plus and minus sign for positive and negative numbers
-                ofRectangle buttonHEX;
+	ofTrueTypeFont testFont;
+	ofSoundPlayer resultSound;
+	//---------KIERAN----------------------
+	ofRectangle button1;
+	ofRectangle buttonEqual;
+	ofRectangle buttonDecimal;
+	ofRectangle button0;
+	ofRectangle historyButton;
+	//-------------------------------------
 
 
-		//--------------------------------------
+	//------------DAN----------------------
+	ofRectangle buttonPlus;
+	ofRectangle buttonX;
+	ofRectangle buttonMinus;
+	ofRectangle buttonDiv;
+	ofRectangle button3;
 
-		//------------ANJALY--------------------
-
-                   ofRectangle button8;
-                   ofRectangle button5;
-                   ofRectangle button2;
-                   ofRectangle button9;
-                   ofRectangle button6;
-                   ofRectangle buttonPercentage;
-                   ofRectangle buttonPi;
+	//--------------------------------------
 
 
-		//--------buttons for the HEX page-----------------
-             //---------------anjaly-------------------------------
-                        ofRectangle buttonA;
-                        ofRectangle buttonB;
-                        ofRectangle buttonC;
-                        ofRectangle buttonD;
-                         ofRectangle buttonE;
-                         ofRectangle buttonF;
-        
-		//------------ANJALI--------------------
-                ofRectangle buttonh1;
-                ofRectangle buttonh2;
-                ofRectangle buttonh3;
-                ofRectangle buttonh4;
-                ofRectangle buttonh5;
-                ofRectangle buttonh6;
-                ofRectangle buttonh7;
-                ofRectangle buttonh8;
-                ofRectangle buttonh9;
-                ofRectangle buttonh0;
-
-                ofRectangle buttonPlusH;
-                ofRectangle buttonXH;
-                ofRectangle buttonMinusH;
-                ofRectangle buttonDivH;
-
-                ofRectangle buttonEqualH;
-
-                ofRectangle buttonClearH;
-
-		//--------------------------------------
+	//------------ANJALI--------------------
+	ofRectangle buttonClear;
+	ofRectangle button7;
+	ofRectangle button4;
+	ofRectangle buttonPandN; // plus and minus sign for positive and negative numbers
+	ofRectangle buttonHEX;
 
 
+	//--------------------------------------
+
+	//------------ANJALY--------------------
+
+	ofRectangle button8;
+	ofRectangle button5;
+	ofRectangle button2;
+	ofRectangle button9;
+	ofRectangle button6;
+	ofRectangle buttonPercentage;
+	ofRectangle buttonPi;
 
 
+	//--------buttons for the HEX page-----------------
+		 //---------------anjaly-------------------------------
+	ofRectangle buttonA;
+	ofRectangle buttonB;
+	ofRectangle buttonC;
+	ofRectangle buttonD;
+	ofRectangle buttonE;
+	ofRectangle buttonF;
 
-		//--------------------------------------
+	//------------ANJALI--------------------
+	ofRectangle buttonh1;
+	ofRectangle buttonh2;
+	ofRectangle buttonh3;
+	ofRectangle buttonh4;
+	ofRectangle buttonh5;
+	ofRectangle buttonh6;
+	ofRectangle buttonh7;
+	ofRectangle buttonh8;
+	ofRectangle buttonh9;
+	ofRectangle buttonh0;
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+	ofRectangle buttonPlusH;
+	ofRectangle buttonXH;
+	ofRectangle buttonMinusH;
+	ofRectangle buttonDivH;
+
+	ofRectangle buttonEqualH;
+
+	ofRectangle buttonClearH;
+
+	//--------------------------------------
+
+
+	//--------------------------------------
+	void mousePressed(int x, int y, int button);
+
+
+	//extra functions for functionality
+	void appendNumber(const string& number);
+	void processOperator(const string& op);
+	void calculate();
+	void percentage();
+	void ValueOfPi();
+	void Clear();
 
 
 
-		//extra functions for functionality
-		void appendNumber(const string& number);
-		void processOperator(const string& op);
-		void calculate();
-                void percentage();
-                void ValueOfPi();
-                void Clear();
+	//variables for functionality.
+	string currentNum;
+	string totalNum;
+	string operatorState;
+	float num1 = 0;
+	float num2 = 0;
+	int currentIndex;
+	vector <float> totals;
 
-      
-	
-		//variables for functionality.
-		string currentNum;
-		string totalNum;
-		string operatorState;
-		float num1 = 0;
-		float num2 = 0;
-		int currentIndex;
-		vector <float> totals;
+	// Declare a member variable to store the running total
+	float runningTotal = 0;
 
-// Declare a member variable to store the running total
-float runningTotal = 0;
+	// Declare a member variable to indicate whether the calculation has been performed
+	bool calculationPerformed = false;
+	bool isNewNumber = true;
+	bool buttonPNClicked = false;
+	bool buttonClicked = false;
+	bool isNegative = false; // Track whether the number is negative or positive
+	bool decimalRepeat = false;
 
-// Declare a member variable to indicate whether the calculation has been performed
-bool calculationPerformed = false;
-bool isNewNumber = true;
-bool buttonPNClicked = false;
-bool buttonClicked = false;
-bool isNegative = false; // Track whether the number is negative or positive
-bool decimalRepeat = false;
-
-//converting hex number to decimal
-ofxJSONElement json;
-string hexToDecimal();
-//converting decimal to hexadecimal 
-string DecimalTohex();
+	//converting hex number to decimal
+	ofxJSONElement json;
+	string hexToDecimal();
+	//converting decimal to hexadecimal 
+	string DecimalTohex();
 };
